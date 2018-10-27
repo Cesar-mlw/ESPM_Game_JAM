41e43c00a39e776218005b3e5f9fe1c84b452c0a
Cesar Moura Leite Westphal <cesarmlwestphal@gmail.com>
2018-10-27 15:13:05 -0300

    Added Player Movement

--- Halloween_Game_Jam/Assets/Scripts/PlayerMovement.cs
+++ Halloween_Game_Jam/Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public Transform FirePoint;
+	public float jetpackForce = 0f;
+
+	Rigidbody rb;
+
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+	}
+
+	private void FixedUpdate() {
+		if(Input.GetKey(KeyCode.LeftControl)){
+			jetpackForce += 0.5f;
+			
+			Shoot(jetpackForce);
+		}
+		else{
+			if(jetpackForce - 0.5f >= 0f){
+				jetpackForce -= 0.5f; 
+			}
+			else{
+				jetpackForce = 0f;
+			}
+		}
+			Rotate();
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Shoot(float jetpackForce){
+		rb.AddRelativeForce(0, 0,  - (rb.transform.localPosition.z - jetpackForce) * Time.deltaTime, ForceMode.Impulse);
+	}
+
+	void Rotate(){
+		float h = 2f * Input.GetAxis("Mouse X");
+
+		rb.transform.Rotate(h, 0, 0);
 	}
 }
